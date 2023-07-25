3bffacc323f3c46b65d78123a1e67cfd459ceada
Rocky <rocky@fepm.com.tw>
2023-07-25 10:46:55 +0800

    [modify] test

--- __tests__/pages/Index.test.tsx
+++ __tests__/pages/Index.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import Home from '../../app/[locale]/page';
 
-test('繁中選項存在於Link內', () => {
+test('英文選項存在於Link內', () => {
   render(<Home />);
 
-  const traditionalChineseButton = screen.getByText('繁中');
+  const traditionalChineseButton = screen.getByText('英文');
   expect(traditionalChineseButton).toBeInTheDocument();
 });
